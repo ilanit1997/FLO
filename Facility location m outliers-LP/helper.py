3c1195f80c4053038f50098b2edf882398ebc347
ilanit1997 <80041689+ilanit1997@users.noreply.github.com>
2021-09-17 10:01:36 +0300

    Update helper.py

--- Facility location m outliers-LP/helper.py
+++ Facility location m outliers-LP/helper.py
@@ -1,100 +1,53 @@
-from pulp import *
-from sklearn.metrics.pairwise import euclidean_distances
-from helper import *
+import matplotlib.pyplot as plt
+from sklearn import datasets
+import pandas as pd
+import numpy as np
+import random
 
 
-def FLO_M_LP(Facilities, Clients, Distances, z, m):
-    n = Distances.shape[0]
-    G = np.zeros((n,n))
-    # Setting the Problem
-    Facilities = ['STATION {}'.format(x) for x in Facilities]
-    prob = LpProblem("Facility Location Problem with m outliers", LpMinimize)
-    # Defining our Desicion Variables
-    # is facility f open
-    use_facility = LpVariable.dicts("Use Facility", Facilities, 0, 1, LpBinary)
-    #is client v served by facility f
-    ser_customer = LpVariable.dicts("Service", [(v,f) for v in Clients for f in Facilities], 0, 1, LpBinary)
-    #is point v outlier
-    outlier_customer = LpVariable.dicts("Outlier", Clients, 0, 1, LpBinary)
-    # Setting the Objective Function
-    #min \sum d_vf*x_vf + z*\sum y_f
-    prob +=  lpSum(Distances[v][int(f.split(' ')[1])]*ser_customer[(v,f)] for f in Facilities for v in Clients)\
-        + z*lpSum(use_facility[f] for f in Facilities)
+def plot_points(paid_facilities, clients, outliers, cost, m, z, P, type='LP'):
+    x_clients, y_clients = [P[0][c] for c in clients], [P[1][c] for c in clients]
+    scatter_clients = plt.scatter(x_clients, y_clients, color='black')
+    x_outliers, y_outliers = [P[0][c] for c in outliers], [P[1][c] for c in outliers]
+    scatter_outliers = plt.scatter(x_outliers, y_outliers, color='red')
+    x_facilities, y_facilities = [P[0][c] for c in paid_facilities], [P[1][c] for c in paid_facilities]
+    scatter_facilities = plt.scatter(x_facilities, y_facilities, color='green')
+    plt.legend((scatter_clients, scatter_outliers, scatter_facilities),
+               ('clients', 'outliers', 'paid facilities'),
+               loc='upper left',
+               fontsize=8)
+    title = 'FLO with {}, with m={} z= {}, COST: {}'.format(type, m,z, cost)
+    plt.title(title)
+    plt.show()
+
+def generate_date(type = 'Boston'):
+    try:
+        if type == 'Boston':
+            boston = datasets.load_boston()
+            boston_df = pd.DataFrame(boston.data)
+            columns = boston.feature_names
+            boston_df.columns = columns
+            x, y = boston_df['INDUS'], boston_df['TAX']
+            P = np.array([np.array([x1, y1]) for x1, y1 in zip(x, y)])
+            P = P[np.unique(P[:, 1], return_index=True)[1]].T
+            P = P[:, :]
+
+            # P = P[:, np.random.permutation(P.shape[1])]
+        elif type == 'Simple':
+            P = np.array([[1,1], [1,1.5], [0.5, 1], [1.4,1.5], [5,5], [2,2], [3,3]]).T
+
+        return P
+
+    except ValueError:
+        print('Invalid data set type')
+        raise
 
 
-    # Costraints
-    # for all v,f : y_f-x_vf >=0
-    for v in Clients:
-        for f in Facilities:
-            prob += use_facility[f] - ser_customer[(v,f)] >=0
-
-    # prob += outlier_customer[57] == 1
-    # prob += outlier_customer[62] == 1
-
-    # for all v: \sum f x_vf + o_v >=1
-    for v in Clients:
-        prob += lpSum(ser_customer[(v,f)] for f in Facilities) + outlier_customer[v] >=1
-
-    # -\sum o_v >= -m
-    prob+= -lpSum(outlier_customer[v] for v in Clients) >= -m
-
-    # forall v,f: x_vf, y_f, o_v >=0
-    for v in Clients:
-        prob += outlier_customer[   v] >= 0 #o_v
-        for f in Facilities:
-            prob += use_facility[f] >=0 # y_f
-            prob += ser_customer[(v,f)] >=0 #x_vf
-
-    # SOLUTION
-    prob.solve()
-    print("Status: ", LpStatus[prob.status])
-    open_facilities, final_clients, outliers = [], [], []
-    # PRINT DECISION VARIABLES
-    TOL = .0001  # tolerance for identifying which locations the algorithm has chosen
-    for f in Facilities:
-        if use_facility[f].varValue > TOL:
-            current_f = int(f.split(' ')[1])
-            open_facilities.append(current_f)
-            print("Establish facility at site: ", current_f)
-
-    for v in Clients:
-        for f in Facilities:
-            if ser_customer[(v,f)].varValue > TOL:
-                current_f = int(f.split(' ')[1])
-                if v not in final_clients:
-                    final_clients.append(v)
-                G[v][current_f] = 1
-                print("client:", v, ' is served by facility:' , current_f)
-
-    for o in Clients:
-        if outlier_customer[o].varValue > TOL:
-            outliers.append(o)
-            print("Establish outlier of index: ", o)
 
 
-    # PRINT OPTIMAL SOLUTION
-    cost = value(prob.objective)
-    print("The total cost of installing and operating facilities = ", cost)
-
-    return open_facilities, final_clients, outliers, cost, G
-
-
-if __name__ == '__main__':
-    num_args = len(sys.argv)
-    if num_args == 1:
-        #generate data with default values
-        types = ['Boston', 'Simple']
-        P = generate_date(type='Simple')
-        z, m = 5, 3
-
-    #insert data from txt file (no header)
-    elif num_args == 3:
-        file_name = sys.argv[1]
-        z = int(sys.argv[2])
-        m = int(sys.argv[3])
-        P = np.loadtxt(file_name)
-
-    D = euclidean_distances(P.T, P.T)
-    Clients, Facilities = list(range(P.shape[1])), list(range(P.shape[1]))
-    open_facilities, final_clients, outliers, cost, G = FLO_M_LP(Facilities, Clients, D, z, m)
-    plot_points(open_facilities, final_clients, outliers, cost, m , z, P)
+def cost_func(clients, facilities, distance, z = -1):
+    total_cost = 0 if z == -1 else z*len(facilities)
+    for c in clients:
+        if len(distance[c, facilities])>0:
+            total_cost+= min(distance[c,facilities])
+    return total_cost
